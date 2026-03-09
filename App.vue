6384fcbdfa76eac0d96b69cdd203b5463137db06
nifemi8 <aremuoluwanifemi06@gmail.com>
2026-03-09 01:58:58 -0500

    Added Vue Component

--- App.vue
+++ App.vue
@@ -1,1 +1,16 @@
+<template>
+  <div>
+    <h1>Hello Vue</h1>
+    <p>This is my Vue homework app.</p>
 
+    <button @click="count++">
+      Clicked {{ count }} times
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
